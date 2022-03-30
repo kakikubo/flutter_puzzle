3bb9cd74fc3a1d10bfde053d8f4b882d4d2e4e8e
kakikubo <kakikubo@gmail.com>
2022-03-31 06:46:27 +0900

    change app name

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,11 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(const PuzzleApp());
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
+class PuzzleApp extends StatelessWidget {
+  const PuzzleApp({Key? key}) : super(key: key);
 
   // This widget is the root of your application.
   @override
